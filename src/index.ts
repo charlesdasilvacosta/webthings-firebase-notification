aaa9839f0634cc1f3f4f7f45817293e991591426
charlesdasilvacosta <dasilvacosta.charles@gmail.com>
2020-07-30 00:35:40 +0200

    Call adapter in index

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
-
+import { FirebaseNotification } from './firebase-notification.adapter';
 
 export = (addonManager: any, manifest: any) => {
-    console.log('intialization firebase notification');
+    // discover(addonManager, manifest);
+    // loadGatewayFromConfig(addonManager, manifest);
+
+    const firebaseNotification = new FirebaseNotification(addonManager, manifest, manifest.name);
 };
